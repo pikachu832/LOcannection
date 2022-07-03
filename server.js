c99254c0cb0325632fd2102cf4c80b71ec23f997
AmitBarash <82405075+AmitBarash@users.noreply.github.com>
2022-07-03 17:56:03 +0300

    Add files via upload
    לא לשכוח לכתוב בטרמינל "NPM INIT" ולהוריד את כל התיקיות

---
+++ server.js
@@ -0,0 +1,40 @@
+// const { query } = require('express');
+const bodyParser = require('body-parser');
+const express = require('express');
+// const res = require('express/lib/response');
+// const { isObjectIdOrHexString } = require('mongoose');
+// const db = require('./mongoConnect');
+
+const app = express();
+
+app.set('view engine' , 'ejs');
+
+// class User{
+//     constructor(userName , password)
+// }
+
+// submit()
+// {
+//     const user = new User({});user.save().then(() => {
+//         isObjectIdOrHexString.emit('site user' , user);
+//     });
+// }
+
+app.listen(3000 , () => {
+    console.log('server is listening on port 3000');
+});
+app.get('/' , (req , res) => {
+    res.status(200).render('homepage');
+});
+app.post('/ans' , (req , res) => {
+    const userName = req.body.username;
+    console.log(userName);
+    res.status(200).render('ans' , {userName});
+});
+app.get('/login' , (req , res) => {
+    res.status(200).render('login');
+});
+// app.use((req , res) => {
+//     res.status(404).send("404 page");
+// });
+
